27cd90fd81ff04fefbaf95ed2db70dfb1d44e532
Lucas <lucas.almdiniz@gmail.com>
2024-08-26 21:23:05 -0400

    feat: Criado componente Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import bannerImg from '../../assets/imagens/banner.png';
+
+import './style.scss';
+
+function Banner() {
+  return (
+    <header className="header">
+      <img src={bannerImg} alt="Banner do site" />
+    </header>
+  );
+}
+
+export default Banner;
