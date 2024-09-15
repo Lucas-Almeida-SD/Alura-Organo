f56273ff1bb7899b6b855a0421b2e719971ce725
Lucas <lucas.almdiniz@gmail.com>
2024-09-15 19:03:31 -0400

    feat: Criado estado para armazenar os dados do colaborador e criada função para alterar o valor do estado

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputText from '../InputText';
 import Select from '../Select';
 import Button from '../Button';
@@ -16,15 +16,56 @@ function Form() {
     'Inovação e Gestão',
   ];
 
+  const [collaborator, setCollaborator] = useState({
+    name: '',
+    role: '',
+    image: '',
+    team: teamList[0],
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setCollaborator({ ...collaborator, [name]: value });
+  };
+
   return (
     <section className="colaborator-section">
       <form className="colaborator-form">
         <h2>Preencha os dados para criar o card do colaborador.</h2>
         <div className="form-input-container">
-          <InputText id="name" label="Nome" placeholder="Digite seu nome" />
-          <InputText id="cargo" label="Cargo" placeholder="Digite seu cargo" />
-          <InputText id="imagem" label="Imagem" placeholder="Informe o endereço da imagem" />
-          <Select dataList={teamList} label="Time" id="team" />
+          <InputText
+            id="form-name"
+            label="Nome"
+            placeholder="Digite seu nome"
+            name="name"
+            value={collaborator.name}
+            handleChange={handleChange}
+          />
+          <InputText
+            id="form-role"
+            label="Cargo"
+            placeholder="Digite seu cargo"
+            name="role"
+            value={collaborator.role}
+            handleChange={handleChange}
+          />
+          <InputText
+            id="form-image"
+            label="Imagem"
+            placeholder="Informe o endereço da imagem"
+            name="image"
+            value={collaborator.image}
+            handleChange={handleChange}
+          />
+          <Select
+            dataList={teamList}
+            label="Time"
+            id="form-team"
+            name="team"
+            value={collaborator.team}
+            handleChange={handleChange}
+          />
           <Button isSubmit label="Criar card" />
         </div>
       </form>
