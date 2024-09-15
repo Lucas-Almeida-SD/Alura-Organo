a01fe4adaed1bd99b89cdd56dff99c83d7be9c14
Lucas <lucas.almdiniz@gmail.com>
2024-09-15 19:04:27 -0400

    feat: Implementado valor do estado do colaborador e função para alterar o valor

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-function InputText({ label, placeholder, id }) {
+function InputText({
+  label, placeholder, id, name, value, handleChange,
+}) {
   return (
     <div className="input-text-container">
       <label htmlFor={id}>{`${label}`}</label>
-      <input id={id} type="text" placeholder={placeholder} />
+      <input
+        id={id}
+        type="text"
+        name={name}
+        value={value}
+        onChange={handleChange}
+        placeholder={placeholder}
+      />
     </div>
   );
 }
@@ -18,4 +27,7 @@ InputText.propTypes = {
   label: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-function Select({ dataList, id, label }) {
+function Select({
+  dataList, id, label, name, value, handleChange,
+}) {
   return (
     <div className="select-container">
       <label htmlFor={id}>{label}</label>
-      <select name={id} id={id}>
+      <select name={name} id={id} value={value} onChange={handleChange}>
         {dataList.map((element) => (
-          <option key={element} value={element.toLowerCase()}>{element}</option>))}
+          <option key={element} value={element}>{element}</option>))}
       </select>
     </div>
   );
@@ -21,4 +23,7 @@ Select.propTypes = {
   dataList: PropTypes.arrayOf(PropTypes.string).isRequired,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
