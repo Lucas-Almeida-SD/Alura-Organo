c34fb4d147e4c75458bd137fed6fbc2b2783efd2
Lucas <lucas.almdiniz@gmail.com>
2024-09-22 11:41:22 -0400

    feat: Criado componente "Team"

---
+++ src/components/Team/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CollaboratorCard from '../CollaboratorCard';
+import './style.scss';
+
+function Team({ team }) {
+  const { name, label, collaborators } = team;
+
+  if (collaborators.length < 1) {
+    return null;
+  }
+
+  return (
+    <section className={`team-section ${name}`}>
+      <h3 className="team-name">{label}</h3>
+      <div className="collaborators-container">
+        {collaborators.map((collaborator) => (
+          <CollaboratorCard key={collaborator.name} collaborator={collaborator} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Team;
+
+Team.propTypes = {
+  team: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    collaborators: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      role: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      team: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
