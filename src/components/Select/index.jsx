bbaeedd2b69a04d0057da001a33fd9dc537ba517
Lucas <lucas.almdiniz@gmail.com>
2024-09-21 22:29:35 -0400

    refactor: prop "datalist" substituida pela prop  "teams"

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 function Select({
-  dataList, id, label, name, value, handleChange,
+  teams, id, label, name, handleChange, value,
 }) {
   return (
     <div className="select-container">
       <label htmlFor={id}>{label}</label>
       <select name={name} id={id} value={value} onChange={handleChange}>
-        {dataList.map((element) => (
-          <option key={element} value={element}>{element}</option>))}
+        {Object.keys(teams).map((teamKey) => (
+          <option key={teamKey} value={teams[`${teamKey}`].name}>{teams[`${teamKey}`].label}</option>))}
       </select>
     </div>
   );
@@ -20,7 +20,78 @@ function Select({
 export default Select;
 
 Select.propTypes = {
-  dataList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  teams: PropTypes.shape({
+    programacao: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    frontEnd: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    dataScience: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    devops: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    uxAndDesign: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    mobile: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+    innovationAndManagement: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      collaborators: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
+      })).isRequired,
+    }),
+  }).isRequired,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
