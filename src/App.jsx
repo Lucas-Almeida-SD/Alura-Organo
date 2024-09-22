cabbaebebda114c16aa094f7085ddaf5f4fd222a
Lucas <lucas.almdiniz@gmail.com>
2024-09-21 22:19:04 -0400

    feat: Elevado nivel do estado "collaborator"; Criado estado "teams"; Estados passados vias props para o componente "Form"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import Banner from './components/Banner';
 import Form from './components/Form';
 
 function App() {
+  const [teams, setTeams] = useState({
+    programacao: { name: 'programacao', label: 'Programação', collaborators: [] },
+    frontEnd: { name: 'frontEnd', label: 'Front-End', collaborators: [] },
+    dataScience: { name: 'dataScience', label: 'Data Science', collaborators: [] },
+    devops: { name: 'devops', label: 'Devops', collaborators: [] },
+    uxAndDesign: { name: 'uxAndDesign', label: 'UX e Design', collaborators: [] },
+    mobile: { name: 'mobile', label: 'Mobile', collaborators: [] },
+    innovationAndManagement: { name: 'innovationAndManagement', label: 'Inovação e Gestão', collaborators: [] },
+  });
+
+  const [collaborator, setCollaborator] = useState({
+    name: '',
+    role: '',
+    image: '',
+    team: teams.programacao.name,
+  });
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form
+        teams={teams}
+        setTeams={setTeams}
+        collaborator={collaborator}
+        setCollaborator={setCollaborator}
+      />
     </div>
   );
 }
