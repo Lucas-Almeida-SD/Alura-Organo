12d483c62137f21ff4c1ce5334c4ca8d1ab15b66
Lucas <lucas.almdiniz@gmail.com>
2024-09-22 12:13:56 -0400

    feat: Criado dataset "teamsDataList"

---
+++ src/utils.js
@@ -0,0 +1,188 @@
+const teamsDataList = {
+  programacao: [
+    {
+      name: 'Bulbasaur',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
+      team: 'programacao',
+    },
+    {
+      name: 'Oddish',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/043.png',
+      team: 'programacao',
+    },
+    {
+      name: 'Bellsprout',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/069.png',
+      team: 'programacao',
+    },
+    {
+      name: 'Chikorita',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/152.png',
+      team: 'programacao',
+    },
+  ],
+  frontEnd: [
+    {
+      name: 'Squirtle',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
+      team: 'frontEnd',
+    },
+    {
+      name: 'Psyduck',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/054.png',
+      team: 'frontEnd',
+    },
+    {
+      name: 'Poliwag',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/060.png',
+      team: 'frontEnd',
+    },
+    {
+      name: 'Tentacool',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/072.png',
+      team: 'frontEnd',
+    },
+  ],
+  dataScience: [
+    {
+      name: 'Caterpie',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/010.png',
+      team: 'dataScience',
+    },
+    {
+      name: 'Weedle',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/013.png',
+      team: 'dataScience',
+    },
+    {
+      name: 'Paras',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/046.png',
+      team: 'dataScience',
+    },
+    {
+      name: 'Venonat',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/048.png',
+      team: 'dataScience',
+    },
+  ],
+  devops: [
+    {
+      name: 'Charmander',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
+      team: 'devops',
+    },
+    {
+      name: 'Vulpix',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/037.png',
+      team: 'devops',
+    },
+    {
+      name: 'Growlithe',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/058.png',
+      team: 'devops',
+    },
+    {
+      name: 'Ponyta',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/077.png',
+      team: 'devops',
+    },
+  ],
+  uxAndDesign: [
+    {
+      name: 'Clefairy',
+      role: 'Desenvolvedora de software e instrutora',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/035.png',
+      team: 'uxAndDesign',
+    },
+    {
+      name: 'Jigglypuff',
+      role: 'Engenheira de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/039.png',
+      team: 'uxAndDesign',
+    },
+    {
+      name: 'Togepi',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/175.png',
+      team: 'uxAndDesign',
+    },
+    {
+      name: 'Mr. Mime',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/122.png',
+      team: 'uxAndDesign',
+    },
+  ],
+  mobile: [
+    {
+      name: 'Pikachu',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png',
+      team: 'mobile',
+    },
+    {
+      name: 'Magnemite',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/081.png',
+      team: 'mobile',
+    },
+    {
+      name: 'Electabuzz',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/125.png',
+      team: 'mobile',
+    },
+    {
+      name: 'Jolteon',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/135.png',
+      team: 'mobile',
+    },
+  ],
+  innovationAndManagement: [
+    {
+      name: 'Mankey',
+      role: 'Desenvolvedor de software e instrutor',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/056.png',
+      team: 'innovationAndManagement',
+    },
+    {
+      name: 'Machop',
+      role: 'Engenheiro de Software ',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/066.png',
+      team: 'innovationAndManagement',
+    },
+    {
+      name: 'Hitmonlee',
+      role: 'Desenvolvedor Python e JavaScript',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/106.png',
+      team: 'innovationAndManagement',
+    },
+    {
+      name: 'Hitmonchan',
+      role: 'Hipster e CEO',
+      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/107.png',
+      team: 'innovationAndManagement',
+    },
+  ],
+};
+
+export default {
+  teamsDataList,
+};
