15535cffabb4e06862b49fbaf0d3f926c28c15c5
Lucas <lucas.almdiniz@gmail.com>
2024-09-24 21:21:19 -0400

    refactor: Importadas imagens dos colaboradores para serem usadas ni dataset

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,56 @@
+import Bulbasaur from './assets/collaborators/Bulbasaur.png';
+import Oddish from './assets/collaborators/Oddish.png';
+import Bellsprout from './assets/collaborators/Bellsprout.png';
+import Chikorita from './assets/collaborators/Chikorita.png';
+import Squirtle from './assets/collaborators/Squirtle.png';
+import Psyduck from './assets/collaborators/Psyduck.png';
+import Poliwag from './assets/collaborators/Poliwag.png';
+import Tentacool from './assets/collaborators/Tentacool.png';
+import Caterpie from './assets/collaborators/Caterpie.png';
+import Weedle from './assets/collaborators/Weedle.png';
+import Paras from './assets/collaborators/Paras.png';
+import Venonat from './assets/collaborators/Venonat.png';
+import Charmander from './assets/collaborators/Charmander.png';
+import Vulpix from './assets/collaborators/Vulpix.png';
+import Growlithe from './assets/collaborators/Growlithe.png';
+import Ponyta from './assets/collaborators/Ponyta.png';
+import Clefairy from './assets/collaborators/Clefairy.png';
+import Jigglypuff from './assets/collaborators/Jigglypuff.png';
+import Togepi from './assets/collaborators/Togepi.png';
+import mrMime from './assets/collaborators/Mr_Mime.png';
+import Pikachu from './assets/collaborators/Pikachu.png';
+import Magnemite from './assets/collaborators/Magnemite.png';
+import Electabuzz from './assets/collaborators/Electabuzz.png';
+import Jolteon from './assets/collaborators/Jolteon.png';
+import Mankey from './assets/collaborators/Mankey.png';
+import Machop from './assets/collaborators/Machop.png';
+import Hitmonlee from './assets/collaborators/Hitmonlee.png';
+import Hitmonchan from './assets/collaborators/Hitmonchan.png';
+
 const teamsDataList = {
   programacao: [
     {
       name: 'Bulbasaur',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
+      image: Bulbasaur,
       team: 'programacao',
     },
     {
       name: 'Oddish',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/043.png',
+      image: Oddish,
       team: 'programacao',
     },
     {
       name: 'Bellsprout',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/069.png',
+      image: Bellsprout,
       team: 'programacao',
     },
     {
       name: 'Chikorita',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/152.png',
+      image: Chikorita,
       team: 'programacao',
     },
   ],
@@ -29,25 +58,25 @@ const teamsDataList = {
     {
       name: 'Squirtle',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png',
+      image: Squirtle,
       team: 'frontEnd',
     },
     {
       name: 'Psyduck',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/054.png',
+      image: Psyduck,
       team: 'frontEnd',
     },
     {
       name: 'Poliwag',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/060.png',
+      image: Poliwag,
       team: 'frontEnd',
     },
     {
       name: 'Tentacool',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/072.png',
+      image: Tentacool,
       team: 'frontEnd',
     },
   ],
@@ -55,25 +84,25 @@ const teamsDataList = {
     {
       name: 'Caterpie',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/010.png',
+      image: Caterpie,
       team: 'dataScience',
     },
     {
       name: 'Weedle',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/013.png',
+      image: Weedle,
       team: 'dataScience',
     },
     {
       name: 'Paras',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/046.png',
+      image: Paras,
       team: 'dataScience',
     },
     {
       name: 'Venonat',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/048.png',
+      image: Venonat,
       team: 'dataScience',
     },
   ],
@@ -81,25 +110,25 @@ const teamsDataList = {
     {
       name: 'Charmander',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png',
+      image: Charmander,
       team: 'devops',
     },
     {
       name: 'Vulpix',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/037.png',
+      image: Vulpix,
       team: 'devops',
     },
     {
       name: 'Growlithe',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/058.png',
+      image: Growlithe,
       team: 'devops',
     },
     {
       name: 'Ponyta',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/077.png',
+      image: Ponyta,
       team: 'devops',
     },
   ],
@@ -107,25 +136,25 @@ const teamsDataList = {
     {
       name: 'Clefairy',
       role: 'Desenvolvedora de software e instrutora',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/035.png',
+      image: Clefairy,
       team: 'uxAndDesign',
     },
     {
       name: 'Jigglypuff',
       role: 'Engenheira de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/039.png',
+      image: Jigglypuff,
       team: 'uxAndDesign',
     },
     {
       name: 'Togepi',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/175.png',
+      image: Togepi,
       team: 'uxAndDesign',
     },
     {
       name: 'Mr. Mime',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/122.png',
+      image: mrMime,
       team: 'uxAndDesign',
     },
   ],
@@ -133,25 +162,25 @@ const teamsDataList = {
     {
       name: 'Pikachu',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png',
+      image: Pikachu,
       team: 'mobile',
     },
     {
       name: 'Magnemite',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/081.png',
+      image: Magnemite,
       team: 'mobile',
     },
     {
       name: 'Electabuzz',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/125.png',
+      image: Electabuzz,
       team: 'mobile',
     },
     {
       name: 'Jolteon',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/135.png',
+      image: Jolteon,
       team: 'mobile',
     },
   ],
@@ -159,25 +188,25 @@ const teamsDataList = {
     {
       name: 'Mankey',
       role: 'Desenvolvedor de software e instrutor',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/056.png',
+      image: Mankey,
       team: 'innovationAndManagement',
     },
     {
       name: 'Machop',
       role: 'Engenheiro de Software ',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/066.png',
+      image: Machop,
       team: 'innovationAndManagement',
     },
     {
       name: 'Hitmonlee',
       role: 'Desenvolvedor Python e JavaScript',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/106.png',
+      image: Hitmonlee,
       team: 'innovationAndManagement',
     },
     {
       name: 'Hitmonchan',
       role: 'Hipster e CEO',
-      image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/107.png',
+      image: Hitmonchan,
       team: 'innovationAndManagement',
     },
   ],
