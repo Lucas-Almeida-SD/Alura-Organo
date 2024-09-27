437155c360290326160fb0cdd9abc5a8282f7758
Lucas <lucas.almdiniz@gmail.com>
2024-09-26 20:59:29 -0400

    feat: Criada constantes que representam as PropTypes

---
+++ src/utils/propTypes.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export const collaboratorPropTypes = {
+  name: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  team: PropTypes.string.isRequired,
+};
+
+export const teamPropTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  collaborators: PropTypes.arrayOf(PropTypes.shape(collaboratorPropTypes)).isRequired,
+};
+
+export const teamsPropTypes = {
+  programacao: PropTypes.shape(teamPropTypes),
+  frontEnd: PropTypes.shape(teamPropTypes),
+  dataScience: PropTypes.shape(teamPropTypes),
+  devops: PropTypes.shape(teamPropTypes),
+  uxAndDesign: PropTypes.shape(teamPropTypes),
+  mobile: PropTypes.shape(teamPropTypes),
+  innovationAndManagement: PropTypes.shape(teamPropTypes),
+};
