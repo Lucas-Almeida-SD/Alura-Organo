855a3d6321bfa7e09c815b444ec717505c2cbba0
Lucas <lucas.almdiniz@gmail.com>
2024-09-28 23:18:19 -0400

    feat: Adicionado input de cor; Criada função para atualizar a propriedade "color" do estado "teams"
    Propriedade "color" do estado "teams" altera o "backgroud-color" da section do time

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import hexRgb from 'hex-rgb';
 
 import CollaboratorCard from '../CollaboratorCard';
 import { teamPropTypes } from '../../utils/propTypes';
+
 import './style.scss';
 
-function Team({ team }) {
-  const { name, label, collaborators } = team;
+function Team({ team, setTeams }) {
+  const {
+    name, label, color, collaborators,
+  } = team;
 
   if (collaborators.length < 1) {
     return null;
   }
 
+  const handleColorChange = ({ target }) => {
+    setTeams((previousValue) => ({ ...previousValue, [name]: { ...team, color: target.value } }));
+  };
+
+  const colorConvert = (opacity) => {
+    const { red, green, blue } = hexRgb(color);
+    return `rgb(${red}, ${green}, ${blue}, ${opacity})`;
+  };
+
   return (
-    <section className={`team-section ${name}`}>
-      <h3 className={`team-name subtitle ${name}`}>{label}</h3>
+    <section
+      className={`team-section ${name}`}
+      style={{ backgroundColor: colorConvert(0.15) }}
+    >
+      <h3 className={`team-name subtitle ${name}`} style={{ borderColor: color }}>{label}</h3>
       <div className="collaborators-container">
+        <input
+          type="color"
+          className="team-color"
+          value={color}
+          onChange={handleColorChange}
+        />
         {collaborators.map((collaborator) => (
-          <CollaboratorCard key={collaborator.name} collaborator={collaborator} />
+          <CollaboratorCard
+            key={collaborator.name}
+            collaborator={collaborator}
+            color={collaborator}
+          />
         ))}
       </div>
     </section>
@@ -28,4 +54,5 @@ export default Team;
 
 Team.propTypes = {
   team: PropTypes.shape(teamPropTypes).isRequired,
+  setTeams: PropTypes.func.isRequired,
 };
