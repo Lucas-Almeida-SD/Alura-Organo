a19105ead68976343dde36528e3e3914523be8ee
Lucas <lucas.almdiniz@gmail.com>
2024-09-30 21:39:52 -0400

    refactor: Componente Form alterado pois agora irá conter o formulário para criar times
    O componente Form conterá os demais formulários

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,90 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import toast from 'react-hot-toast';
 
 import { collaboratorPropTypes, teamsPropTypes } from '../../utils/propTypes';
-import InputText from '../InputText';
-import Select from '../Select';
-import Button from '../Button';
 
 import './style.scss';
+import CreateCollaboratorForm from '../CreateCollaboratorForm';
 
 function Form({
   teams, setTeams, collaborator, setCollaborator, showForm,
 }) {
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-
-    setCollaborator({ ...collaborator, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const {
-      [Object.keys(teams)[0]]: { name: teamName },
-      [collaborator.team]: team,
-    } = teams;
-
-    setTeams({
-      ...teams,
-      [collaborator.team]: { ...team, collaborators: [...team.collaborators, collaborator] },
-    });
-
-    setCollaborator({
-      name: '',
-      role: '',
-      image: '',
-      team: teamName,
-      id: crypto.randomUUID(),
-    });
-
-    toast.success('Colaborador cadastrado com sucesso!');
-  };
-
   return (
-    <section className={`colaborator-section ${showForm[0]}`}>
-      <form className="colaborator-form" onSubmit={handleSubmit}>
-        <h2 className="title">Preencha os dados para criar o card do colaborador.</h2>
-        <div className="form-input-container">
-          <InputText
-            id="form-name"
-            label="Nome"
-            placeholder="Digite seu nome"
-            name="name"
-            value={collaborator.name}
-            handleChange={handleChange}
-            isRequired
-          />
-          <InputText
-            id="form-role"
-            label="Cargo"
-            placeholder="Digite seu cargo"
-            name="role"
-            value={collaborator.role}
-            handleChange={handleChange}
-            isRequired
-          />
-          <InputText
-            id="form-image"
-            label="Imagem"
-            placeholder="Informe o endereço da imagem"
-            name="image"
-            value={collaborator.image}
-            handleChange={handleChange}
-            isRequired
-          />
-          <Select
-            teams={teams}
-            label="Time"
-            id="form-team"
-            name="team"
-            value={collaborator.team}
-            handleChange={handleChange}
-          />
-          <Button isSubmit label="Criar card" />
-        </div>
-      </form>
+    <section className={`create-form-section ${showForm[0]}`}>
+      <CreateCollaboratorForm
+        collaborator={collaborator}
+        setCollaborator={setCollaborator}
+        setTeams={setTeams}
+        teams={teams}
+      />
     </section>
   );
 }
