b2e9ab0233b8da82aaad5afc8e48f52557739ce8
Lucas <lucas.almdiniz@gmail.com>
2024-10-05 01:00:37 -0400

    refactor: Adicionado classname dinamico e adicionado evento de "OnCLick" no botão

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
-function Button({ isSubmit, label }) {
+function Button({
+  isSubmit = false, label, handleClick = false, className = '',
+}) {
   return (
-    <button className="button-form text" type={isSubmit ? 'submit' : 'button'}>{label}</button>
+    <button
+      className={`button-form text${className ? ` ${className}` : ''}`}
+      type={isSubmit ? 'submit' : 'button'}
+      onClick={handleClick || (() => {})}
+    >
+      {label}
+    </button>
   );
 }
 
 export default Button;
 
 Button.propTypes = {
-  isSubmit: PropTypes.bool.isRequired,
+  isSubmit: PropTypes.bool,
   label: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
+  className: PropTypes.string,
 };
