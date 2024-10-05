f99f67d8068814af7aecfe6362b3a505460cc6c3
Lucas <lucas.almdiniz@gmail.com>
2024-10-05 01:02:45 -0400

    feat: Adicionado botões para controlar a exibição dos formulários
    Criada função handleClickSelectCreateForm para controlar a renderição condicional dos formulários

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { collaboratorPropTypes, teamsPropTypes } from '../../utils/propTypes';
@@ -6,21 +6,49 @@ import { collaboratorPropTypes, teamsPropTypes } from '../../utils/propTypes';
 import './style.scss';
 import CreateCollaboratorForm from '../CreateCollaboratorForm';
 import CreateTeamForm from '../CreateTeamForm';
+import Button from '../Button';
 
 function Form({
   teams, setTeams, collaborator, setCollaborator, showForm,
 }) {
+  const COLLABORATOR_FORM = 'COLLABORATOR_FORM';
+  const TEAM_FORM = 'TEAM_FORM';
+  const SELECTED = 'selected';
+
+  const [formSelected, setFormSelected] = useState(COLLABORATOR_FORM);
+
+  const handleClickSelectCreateForm = (value) => {
+    setFormSelected(value);
+  };
+
   return (
     <section className={`create-form-section ${showForm[0]}`}>
-      <CreateCollaboratorForm
-        collaborator={collaborator}
-        setCollaborator={setCollaborator}
-        setTeams={setTeams}
-        teams={teams}
-      />
-      <CreateTeamForm
-        setTeams={setTeams}
-      />
+      <div className="form-create-select-container">
+        <Button
+          label="Novo coloborador"
+          className={formSelected === COLLABORATOR_FORM ? SELECTED : ''}
+          handleClick={() => handleClickSelectCreateForm(COLLABORATOR_FORM)}
+        />
+        <Button
+          label="Novo time"
+          className={formSelected === TEAM_FORM ? SELECTED : ''}
+          handleClick={() => handleClickSelectCreateForm(TEAM_FORM)}
+        />
+      </div>
+      {
+        formSelected === COLLABORATOR_FORM ? (
+          <CreateCollaboratorForm
+            collaborator={collaborator}
+            setCollaborator={setCollaborator}
+            setTeams={setTeams}
+            teams={teams}
+          />
+        ) : (
+          <CreateTeamForm
+            setTeams={setTeams}
+          />
+        )
+      }
     </section>
   );
 }
